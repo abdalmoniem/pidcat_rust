62c417460ec6fcfbe691d28e4efcc3d306ed0d32
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2026-01-14 14:37:08 +0200

    fix: writer not using write function from WriterTarget
    Change-Id: I6fdc3e4e68e93d8db0dd3ef7db067740a0346337
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

--- src/controller/writer.rs
+++ src/controller/writer.rs
@@ -1,22 +1,40 @@
-use colored::*;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Result as FmtResult;
+
 use std::fs::File;
-use std::io::{self, Write};
+
+use std::io::Result;
+use std::io::Stdout;
+use std::io::Write;
+use std::io::stdout;
+
+use crate::ValueOrPanic;
 
 #[derive(Debug)]
-pub enum WriterTarget {
-    Console(io::Stdout),
+enum WriterTarget {
+    Console(Stdout),
     File(File),
 }
 
-impl Write for WriterTarget {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+impl Display for WriterTarget {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FmtResult {
         match self {
-            Self::Console(stdout) => stdout.write(buf),
-            Self::File(file) => file.write(buf),
+            Self::Console(stdout) => write!(formatter, "{stdout:?}"),
+            Self::File(file) => write!(formatter, "{file:?}"),
+        }
+    }
+}
+
+impl Write for WriterTarget {
+    fn write(&mut self, buffer: &[u8]) -> Result<usize> {
+        match self {
+            Self::Console(stdout) => stdout.write_all(buffer).map(|_| buffer.len()),
+            Self::File(file) => file.write_all(buffer).map(|_| buffer.len()),
         }
     }
 
-    fn flush(&mut self) -> io::Result<()> {
+    fn flush(&mut self) -> Result<()> {
         match self {
             Self::Console(stdout) => stdout.flush(),
             Self::File(file) => file.flush(),
@@ -28,7 +46,7 @@ impl Write for WriterTarget {
 pub struct Writer {
     pub width: i16,
     pub show_colors: bool,
-    pub target: WriterTarget,
+    target: WriterTarget,
 }
 
 impl Writer {
@@ -36,7 +54,7 @@ impl Writer {
         Self {
             width,
             show_colors,
-            target: WriterTarget::Console(io::stdout()),
+            target: WriterTarget::Console(stdout()),
         }
     }
 
@@ -49,18 +67,12 @@ impl Writer {
     }
 
     pub fn write(&mut self, text: &str) {
-        let res = write!(self.target, "{text}");
-
-        if let Err(err) = res {
-            eprintln!("{}", err.to_string().red().bold());
-        }
+        let err_msg = format!("Failed to write to {}", self.target);
+        self.target.write(text.as_bytes()).unwrap_or_panic(&err_msg);
     }
 
     pub fn flush(&mut self) {
-        let res = self.target.flush();
-
-        if let Err(err) = res {
-            eprintln!("{}", err.to_string().red().bold());
-        }
+        let err_msg = format!("Failed to flush {}", self.target);
+        self.target.flush().unwrap_or_panic(&err_msg);
     }
 }
